9f07997f460d67658e5e6651510c9b146e8b77db
coskunrumeysa <70705198+coskunrumeysa@users.noreply.github.com>
2022-05-30 10:00:05 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//Beni Yaratan Elbet Yolumu Gösterir .Şuara 78 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number1;
+
+            Console.WriteLine("Please enter a number :");
+            number1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Entried number :" + " " + number1);
+
+            Console.Read();
+        }
+    }
+}
